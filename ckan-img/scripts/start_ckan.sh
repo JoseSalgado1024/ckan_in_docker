18b2adaa636f38c9582669604bb46da994548c16
josesalgado1024 <jose.salgado.wrk@gmail.com>
2016-09-11 10:43:39 -0300

    .

--- ckan-img/scripts/start_ckan.sh
+++ ckan-img/scripts/start_ckan.sh
@@ -1,18 +1,15 @@
 #!/bin/bash
-# ec2-52-203-114-238.compute-1.amazonaws.com
 
 eval "/bin/bash $CKAN_INIT/.make_conf.sh"
 eval "/bin/bash $CKAN_INIT/.init_db.sh"
 exit_code=$?
 if [ "$exit_code" -eq "0" ] ; then
-	sed "s/config_filepath = os.path.join(os.path.dirname(os.path.abspath(__file__)), \'producction.ini\')/config_filepath = os.path.join(os.path.dirname(os.path.abspath(__file__)), \'$CKAN_CONFIG_FILE\')/g" $CKAN_CONFIG/apache.wsgi > $CKAN_CONFIG/apache.wsgi.tmp && mv $CKAN_CONFIG/apache.wsgi.tmp $CKAN_CONFIG/apache.wsgi
+	sed "s/producction.ini/$CKAN_CONFIG_FILE/g" $CKAN_CONFIG/apache.wsgi 	
 	service apache2 stop 
 	service apache2 start
 	service apache2 reload
 	service nginx restart
 	while true; do sleep 1000; done
-	# $CKAN_HOME/bin/paster serve "${CKAN_CONFIG}/${CKAN_CONFIG_FILE}" &
-	# $CKAN_HOME/bin/
 else
 	echo "Fallo inicio de CKAN" 
 fi
