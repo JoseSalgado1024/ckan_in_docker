f267dc737736d92c63d607764d929041cd61c619
josesalgado1024 <tierengelmsk@gmail.com>
2016-09-15 18:40:28 -0300

    .

--- auto-deploy/rhel-centos_auto-deploy.sh
+++ auto-deploy/rhel-centos_auto-deploy.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 source docker_friendy_functions.sh
 
-echo "++----------------------------------------------+";
-echo "|                                               |";
-echo "|     BIENVENIDO A LA INSTALACION AUTOMATICA    |";
-echo "|               DE CKAN EN DOCKER               |";
-echo "|                                               |";
-echo "+-----------------------------------------------+";
+printf "\n\x1B[32m++----------------------------------------------+\n";
+printf "\x1B[32m|                                               \x1B[32m|\n";
+printf "\x1B[32m|     \x1B[37mBIENVENIDO A LA INSTALACION AUTOMATICA    \x1B[32m|\n";
+printf "\x1B[32m|               \x1B[37mDE CKAN EN DOCKER               \x1B[32m|\n";
+printf "\x1B[32m|                                        \x1B[34mXX\x1B[37mXX\x1B[34mXX \x1B[32m|\n";
+printf "\x1B[32m+-----------------------------------------------+\n\n";
 # Esta docker insalado?	
 if [ $(dpkg-query -W -f='${Status}' docker-engine 2>/dev/null | grep -c "ok installed") -eq 0 ];
 then
