5234d4d59d90208bdd1608ff4180ed1f840cd07a
josesalgado1024 <tierengelmsk@gmail.com>
2016-09-16 12:24:28 -0300

    .

--- auto-deploy/ubuntu-debian_auto-deploy.sh
+++ auto-deploy/ubuntu-debian_auto-deploy.sh
@@ -4,7 +4,7 @@ source docker_friendy_functions.sh
 printf "\n\x1B[32m++----------------------------------------------+\n";
 printf "\x1B[32m|                                               \x1B[32m|\n";
 printf "\x1B[32m|     \x1B[37mBIENVENIDO A LA INSTALACION AUTOMATICA    \x1B[32m|\n";
-printf "\x1B[32m|               \x1B[37mDE CKAN EN DOCKER               \x1B[32m|\n";
+printf "\x1B[32m|        \x1B[37mDE CKAN EN DOCKER UBUNTU|DEBIAN        \x1B[32m|\n";
 printf "\x1B[32m|                                        \x1B[34mXX\x1B[37mXX\x1B[34mXX \x1B[32m|\n";
 printf "\x1B[32m+-----------------------------------------------+\n\n";
 # Esta docker insalado?	
