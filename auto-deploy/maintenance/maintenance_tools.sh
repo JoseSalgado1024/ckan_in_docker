11610f10db98ca38cbd3f0df2cce5cc374707594
josesalgado1024 <jose.salgado.wrk@gmail.com>
2016-10-16 23:02:52 -0300

    Herramientas de mantenimiento, maqueta

---
+++ auto-deploy/maintenance/maintenance_tools.sh
@@ -0,0 +1,115 @@
+#!/bin/bash
+#
+# TODO LIST
+#
+#   manetenimiento 
+#        usuarios
+#            add
+#            delete
+#            list
+#
+#        actualizacion
+#            horario
+#            activar/desactivar
+#
+#        bases-de-datos
+#            dump
+#            init 
+#            clear
+#
+#        backup
+#            horario
+#            activar/desactivar
+#            destino
+#
+#       app
+#           start
+#            stop
+#            restart
+
+
+.mostrar_ayuda (){
+printf \
+"\n
++---------------------------------------------------+
+|    Herramientas simplificadas de manetenimiento   |
++---------------------------------------------------+
+
+USUARIOS:
+Crear, listar o borrar usuarios de CKAN: --usuario=accion parametro o -u=accion parametro 
+
+    Agregar usuario admin: --usuario=add nombre_de_usuario o -u=add nombre_de_usuario     
+    Listar usuarios de CKAN: --usuario=listar o -u=listar     
+    Borrar usuario: --usuario=del nombre_de_usuario o -u=del nombre_de_usuario    
+
+CKAN:
+Iniciar, detener, reiniciar o desinstalar CKAN: --ckan=accion o -ckan=accion 
+
+    Iniciar CKAN: --ckan=start     
+    Detener CKAN: --ckan=stop
+    Reiniciar CKAN: --ckan=restart    
+    Desinstalar CKAN: --ckan=remove
+
+BASES DE DATOS:
+Tareas de mantenimiento para las bases de datos de CKAN: --bases-de-datos=accion o -db=accion 
+
+    Inicializar bases de datos de CKAN: --bases-de-datos=init o -db=init     
+    Hacer dump de bases de CKAN: --bases-de-datos=dump /carpeta/dump.sql o -db=dump /carpeta/dump.sql 
+    Limpiar Bases de Datos de CKAN: --bases-de-datos=clear o -db=clear
+
+ACTUALIZACION:
+Programar momento de actualizacion para contenedores de CKAN: --actualizacion=accion o -a=accion 
+
+    Activar actualizacion: --actualizacion=start     
+    Desactivar actualizacion: --actualizacion=stop
+
+BACKUP:
+Activar, desactivar backups automaticos para el contenido de CKAN: --backup=accion parametro o -bu=accion parametro 
+
+    Activar backup: --backup=start /carpeta/de/destino o -bu=start /carpeta/de/destino      
+    Desactivar backup: --backup=stop o -bu=stop
+
+\n" 
+}
+
+
+DEFAULT=NOT
+ACTION="NONE"
+VALUE="NONE"
+
+case $1 in
+    -u=*|--usuario=*)
+    ACTION="Usuarios"
+    VALUE="${1#*=}"
+    ;;
+
+    -a=*|--actualizar=*)
+    ACTION="Actualizacion"
+    VALUE="${1#*=}"
+    ;;
+
+    -db=*|--bases-de-datos=*)
+    ACTION="Base de Datos"
+    VALUE="${1#*=}"
+    ;;
+
+    -ckan=*|--ckan=*)
+    ACTION="CKAN"
+    VALUE="${1#*=}"
+    ;;
+
+    -b=*|--backup=*)
+    ACTION="Backup"
+    VALUE="${1#*=}"
+    ;;
+
+    -h | --help)
+    ACTION="Ayuda"
+    VALUE="Mostrar ayuda"
+    .mostrar_ayuda
+    ;;
+esac
+
+if echo "$ACTION" | grep "NONE"; then 
+    echo "Opcion ingresada no valida..."
+fi 
