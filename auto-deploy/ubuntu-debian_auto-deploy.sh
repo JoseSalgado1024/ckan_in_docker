2fd8b0dc9f28354773c49c02e6206e3c62048953
jsalgado <jose.salgado.wrk@gmail.com>
2016-10-18 16:09:00 -0300

    .

--- auto-deploy/ubuntu-debian_auto-deploy.sh
+++ auto-deploy/ubuntu-debian_auto-deploy.sh
@@ -20,9 +20,9 @@ sleep 1
 if [[ $? -eq 0 ]]; then
 	# OK! veamos como quedo tu portal! :D
 	printf "[OK]\nTodo listo! el portal esta funcionando! :D\n "
-	sleep 5
-	bind_portal
-	print_ckan_status
+	#sleep 5
+	#bind_portal
+	#print_ckan_status
 else
 	printf "[FALLO]\nOops... Algo se rompio...\n"
 fi
